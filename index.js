4dcbf79cf8b40f30f83d0509c8469f05f692b581
Tom Sullivan <tom@msbit.com.au>
2018-10-13 12:26:20 +1100

    First blush POC

--- index.js
+++ index.js
@@ -1,7 +1,40 @@
+const bodyParser = require('body-parser');
 const express = require('express');
-const app = express();
+const request = require('request');
+
+const sanitisedRequestHeaders = require('./sanitised-request-headers.js');
+
 const port = 3000;
 
-app.get('/', (request, response) => response.send('Hello World'));
+const app = express();
 
-app.listen(port, () => console.log(`Running on ${port}`));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', (req, res) => {
+  res.send('<form action="/headers" method="post"><input name="url" type="text"/><input type="submit" value="GO"/></form>');
+});
+
+app.get('/headers', (req, res) => res.redirect('/'));
+app.post('/headers', (req, res) => {
+  const options = {
+    headers: sanitisedRequestHeaders(req, req.headers),
+    method: 'head',
+    url: req.body.url
+  };
+  request.head(options, (error, response, body) => {
+    if (error) {
+      res.status(500).send({ error: error });
+    } else {
+      let body = '<table>';
+      Object.keys(response.headers).forEach((key) => {
+        body += `<tr><td>${key}</td><td>${response.headers[key]}</td></tr>`;
+      });
+      body += '</table>';
+      res.send(body);
+    }
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Running on ${port}`);
+});
